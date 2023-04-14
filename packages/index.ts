3cc589431d324e6f4ce8d2ad3bd5fe05f370d52d
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-04-14 09:13:29 +0200

    fix: export button, and input components (#18)

--- packages/index.ts
+++ packages/index.ts
@@ -1,9 +1,11 @@
 export * from './_helpers';
 export * from './alert/src';
+export * from './button/src';
+export * from './textarea/src';
+export * from './textfield/src';
 /* export * from './box/src';
 export * from './breadcrumbs/src';
 export * from './attention/src';
-export * from './button/src';
 export * from './pill/src';
 export * from './card/src';
 export * from './combobox/src';
@@ -14,6 +16,4 @@ export * from './slider/src';
 export * from './steps/src';
 export * from './switch/src';
 export * from './tabs/src';
-export * from './textarea/src';
-export * from './textfield/src';
 export * from './toggle/src'; */
