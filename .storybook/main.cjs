d0ba4b18a6d506b6e59b3a1a3b71289f9d245d65
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-05-05 08:47:25 +0200

    fix: upgrade storybook (#27)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,36 +1,39 @@
 module.exports = {
-    core: {
-        builder: 'webpack5',
-    },
-    stories: ['../packages/**/*.stories.[tj]sx'],
-    addons: ['@storybook/addon-actions/register',/* '@storybook/addon-postcss'*/],
-    framework: '@storybook/react',
-    webpackFinal: async (config) => {
-        config.resolve.alias['@warp-ds/core/attention'] = require.resolve(
-            '../node_modules/@warp-ds/core/dist/attention/index.js',
-        );
-        config.module.rules.push({
-            test: /\.js$/,
-            include: /node_modules/,
-            use: [
-                {
-                    loader: require.resolve('babel-loader'),
-                    options: {
-                        presets: [require('@babel/preset-env').default],
-                    },
-                },
-            ],
-        });
-        config.resolve.extensions.push('.js');
-
-        const uno = (await import('@unocss/webpack'));
-        const { presetWarp } = (await import('@warp-ds/uno'));
-        const { classes } = (await import('@warp-ds/component-classes/classes'));
-
-        config.plugins.push(uno.default({
-            presets: [presetWarp({ usePreflight: true })],
-            safelist: classes,
-        }));
-        return config;
-    },
+  stories: ['../packages/**/*.stories.[tj]sx'],
+  addons: ['@storybook/addon-actions/register'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  webpackFinal: async config => {
+    config.resolve.alias['@warp-ds/core/attention'] = require.resolve('../node_modules/@warp-ds/core/dist/attention/index.js');
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules/,
+      use: [{
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [require('@babel/preset-env').default]
+        }
+      }]
+    });
+    config.resolve.extensions.push('.js');
+    const uno = await import('@unocss/webpack');
+    const {
+      presetWarp
+    } = await import('@warp-ds/uno');
+    const {
+      classes
+    } = await import('@warp-ds/component-classes/classes');
+    config.plugins.push(uno.default({
+      presets: [presetWarp({
+        usePreflight: true
+      })],
+      safelist: classes
+    }));
+    return config;
+  },
+  docs: {
+    autodocs: true
+  }
 };
