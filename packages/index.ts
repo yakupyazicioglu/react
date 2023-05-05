7b1e21ce8af2c34190890f9a3f8256fd6d2f0dc0
Balbina K <41303231+BalbinaK@users.noreply.github.com>
2023-05-05 10:20:10 +0200

    fix(index.ts): export expandable, box and select from packages/index.ts (#30)
    Include all completed components in the build step

--- packages/index.ts
+++ packages/index.ts
@@ -5,14 +5,15 @@ export * from './button/src';
 export * from './steps/src';
 export * from './textarea/src';
 export * from './textfield/src';
-/* export * from './box/src';
+export * from './box/src';
+export * from './expandable/src'
+export * from './select/src';
+/*
 export * from './attention/src';
 export * from './pill/src';
 export * from './card/src';
 export * from './combobox/src';
-export * from './expandable/src';
 export * from './modal/src';
-export * from './select/src';
 export * from './slider/src';
 export * from './switch/src';
 export * from './tabs/src';
