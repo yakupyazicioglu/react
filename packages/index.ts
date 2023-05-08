65211a79049078e60bc8204b330c1cf431f3de6b
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-05-08 11:08:02 +0200

    fix: classes for toggle (#26)

--- packages/index.ts
+++ packages/index.ts
@@ -3,13 +3,14 @@ export * from './alert/src';
 export * from './box/src';
 export * from './breadcrumbs/src';
 export * from './button/src';
-export * from './expandable/src'
+export * from './expandable/src';
 export * from './pill/src';
-export * from './select/src';
-export * from './steps/src';
 export * from './textarea/src';
 export * from './textfield/src';
-/*
+export * from './select/src';
+export * from './steps/src';
+export * from './toggle/src';
+/* export * from './box/src';
 export * from './attention/src';
 export * from './card/src';
 export * from './combobox/src';
@@ -17,4 +18,4 @@ export * from './modal/src';
 export * from './slider/src';
 export * from './switch/src';
 export * from './tabs/src';
-export * from './toggle/src'; */
+*/
--- packages/toggle/src/component.tsx
+++ packages/toggle/src/component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { label as ccLabel, helpText as ccHelpText, toggle as ccToggle } from '@warp-ds/component-classes';
 import { useId } from '../../utils/src';
 import { ToggleEntry, ToggleProps } from './props';
 import { classNames } from '@chbphone55/classnames';
@@ -8,13 +9,14 @@ function Title({ id, isInvalid, title, optional }) {
   return (
     <legend
       id={`${id}__title`}
-      className={classNames('field-label', {
-        'text-danger': isInvalid,
+      className={classNames({
+        [ccLabel.label]: true,
+        [ccLabel.labelInvalid]: isInvalid,
       })}
     >
       {title}
       {optional && (
-        <span className="pl-8 font-normal text-14 text-gray-500">
+        <span className={ccLabel.optional}>
           (valgfritt)
         </span>
       )}
@@ -26,7 +28,10 @@ function HelpText({ isInvalid, helpId, helpText }: any) {
   return (
     <div
       id={helpId}
-      className={classNames('field-hint', { 'text-danger': isInvalid })}
+      className={classNames({
+        [ccHelpText.helpText]: true,
+        [ccHelpText.helpTextInvalid]: isInvalid
+      })}
     >
       {helpText}
     </div>
@@ -38,10 +43,36 @@ export function Toggle(props: ToggleProps) {
 
   const helpId = props.helpText ? `${id}__hint` : undefined;
   const isInvalid = props.invalid;
-  const isRadioGroup = props.type === 'radio' || props.type === 'radio-button';
-
+  const isRadio = props.type === 'radio';
+  const isCheckbox = props.type === 'checkbox';
+  const isRadioButton = props.type === 'radio-button';
+  const isRadioGroup = isRadio || isRadioButton;
   const isControlled =
-    props.selected !== undefined || props.checked !== undefined;
+    props.selected !== undefined || props.checked !== undefined;  
+
+  const labelClasses = classNames({
+    [ccToggle.label]: !isRadioButton,
+    [ccToggle.focusable]: !isRadioButton,
+    [ccToggle.noContent]: !props.indeterminate,
+    [ccToggle.indeterminate]: props.indeterminate,
+    [`${ccToggle.radio} ${ccToggle.labelRadioColors}`]: isRadio,
+    [ccToggle.radioInvalid]: isRadio && isInvalid,
+    [`${ccToggle.checkbox} ${ccToggle.labelCheckboxColors}  ${ccToggle.checkboxChecked}`]: isCheckbox,
+    [ccToggle.icon]: isCheckbox && !props.indeterminate,
+    [ccToggle.checkboxInvalid]: isCheckbox && isInvalid,
+    [ccToggle.scLabel]: isRadioButton,
+    [ccToggle.scLabelJustified]: props.equalWidth,
+    [ccToggle.scLabelSmall]: props.small,
+  });
+  const inputClasses = classNames({
+    [ccToggle.input]: true,
+  });
+
+  const wrapperClasses = classNames(props.className, {
+    [ccToggle.wrapper]: true,
+    [`${ccToggle.segmentControl} ${ccToggle.focusableWithin}`]: isRadioButton,
+    [ccToggle.scJustified]: props.equalWidth,
+  });
 
   return (
     <fieldset
@@ -49,7 +80,7 @@ export function Toggle(props: ToggleProps) {
       aria-invalid={isRadioGroup ? isInvalid : undefined}
       aria-errormessage={isRadioGroup && isInvalid ? helpId : undefined}
       aria-describedby={helpId}
-      className="field"
+      className={ccToggle.field}
     >
       {props.title && (
         <Title
@@ -60,17 +91,14 @@ export function Toggle(props: ToggleProps) {
         />
       )}
       <div
-        className={classNames(props.className, {
-          'segment-control': props.type === 'radio-button',
-          'segment-control--justified': props.equalWidth,
-          'segment-control--small': props.small,
-          'input-toggle': props.type === 'radio' || props.type === 'checkbox',
-        })}
+        className={wrapperClasses}
       >
         {!props.options && props.label ? (
           <Item
             controlled={isControlled}
             label={props.label}
+            inputClassName={inputClasses}
+            labelClassName={labelClasses}
             checked={props.checked}
             defaultChecked={props.defaultChecked}
             indeterminate={props.indeterminate}
@@ -92,6 +120,8 @@ export function Toggle(props: ToggleProps) {
               defaultChecked={props.defaultSelected?.some(
                 (s) => s.value === option.value,
               )}
+              inputClassName={inputClasses}
+              labelClassName={labelClasses}  
               option={option}
               // @ts-ignore TODO: typecheck
               onChange={(e: ToggleEntry) => props.onChange(e)}
@@ -101,6 +131,7 @@ export function Toggle(props: ToggleProps) {
               helpId={helpId}
               type={isRadioGroup ? 'radio' : 'checkbox'}
               noVisibleLabel={props.noVisibleLabel}
+              multiple
             />
           ))
         )}
