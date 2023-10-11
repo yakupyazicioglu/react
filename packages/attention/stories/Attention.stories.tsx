05885644551b77913285a35faa2266b1b16a0138
felicia-haggqvist <144792260+felicia-haggqvist@users.noreply.github.com>
2023-10-11 15:28:28 +0200

    fix: a11y issues for attention component (#123)
    * add aria-expanded, aria-controls, role="img" + ariaLabel prop to Attention component
    
    * refactor Attention component to show tooltip onFocus
    
    * set ariaLabel + ariaRole as optional to avoid breaking change

--- packages/attention/stories/Attention.stories.tsx
+++ packages/attention/stories/Attention.stories.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Attention } from '../src';
-import { Box } from '../../box/src';
-import { Button } from '../../button/src';
+import React from "react";
+import { Attention } from "../src";
+import { Box } from "../../box/src";
+import { Button } from "../../button/src";
 
-const metadata = { title: 'Overlays/Attention' };
+const metadata = { title: "Overlays/Attention" };
 export default metadata;
 
 export function Callout() {
@@ -12,7 +12,13 @@ export function Callout() {
       <Box info>
         <h1>I am a box full of info</h1>
       </Box>
-      <Attention callout placement="right" isShowing={true}>
+      <Attention
+        callout
+        placement="right"
+        ariaLabel="Green speech bubble pointing left"
+        ariaRole="img"
+        isShowing={true}
+      >
         <p style={{ width: 200 }}>
           I'm a callout because that box over there is new or something
         </p>
@@ -25,23 +31,38 @@ export function Tooltip() {
   const [show, setShow] = React.useState(false);
   const targetEl = React.useRef();
 
+  const handleKeyDown = (event) => {
+    if (event.key === "Escape") {
+      setShow(false);
+    }
+  };
+
   return (
     <div>
       <h1
         className="w-max mb-0"
         ref={targetEl}
+        aria-describedby="tooltip-example"
+        aria-hidden={!show}
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
+        onFocus={() => setShow(true)}
+        onBlur={() => setShow(false)}
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
       >
         hover this for useless info
       </h1>
       <Attention
         tooltip
+        ariaDescribedby="tooltip-example"
+        ariaLabel="Black speech bubble pointing up"
+        ariaRole="tooltip"
         placement="bottom"
         targetEl={targetEl}
         isShowing={show}
       >
-        <p>lol i am a popover</p>
+        <p>lol i am a tooltip</p>
       </Attention>
     </div>
   );
@@ -58,9 +79,9 @@ export function Popover() {
         setShow(false);
       }
     }
-    document.addEventListener('mousedown', onBlurHandler);
+    document.addEventListener("mousedown", onBlurHandler);
     return () => {
-      document.removeEventListener('mousedown', onBlurHandler);
+      document.removeEventListener("mousedown", onBlurHandler);
     };
   });
 
@@ -68,6 +89,8 @@ export function Popover() {
     <div ref={containerRef}>
       <Button
         small
+        aria-expanded={show}
+        aria-controls="pop-over-attention-example"
         utility
         onClick={() => setShow(!show)}
         className="w-max mb-0"
@@ -77,6 +100,8 @@ export function Popover() {
       </Button>
       <Attention
         popover
+        ariaLabel="White speech bubble pointing up"
+        ariaRole="img"
         placement="bottom"
         targetEl={targetEl}
         isShowing={show}
