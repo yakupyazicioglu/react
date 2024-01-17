f9377d49fe7c87bfe9ef453110bd3d5a0e8cd7d4
BalbinaK <BalbinaK@users.noreply.github.com>
2024-01-17 13:54:58 +0000

    Deploy preview for PR 192 🛫

---
+++ pr-preview/pr-192/assets/Alert.stories-R1MJu6T4.js
@@ -0,0 +1,80 @@
+import{j as n,a as c}from"./index-1PzaImZP.js";import{R as s}from"./index-Ainc_9Qe.js";import{B as m}from"./component-tZV1htg5.js";import{r as q}from"./index.m-0-FAh1ao.js";import{E as D}from"./unstyled-heading-m9wKHSOb.js";import{i as o}from"./index-fnlwIS5b.js";import"./i18n-G6_82uWZ.js";import"./item-WJwOQLcN.js";import"./useId-fJNbyo74.js";import"./close-16-3rOtYtoW.js";var U={},Y=Object.defineProperty,f=Object.getOwnPropertySymbols,K=Object.prototype.hasOwnProperty,z=Object.prototype.propertyIsEnumerable,g=(e,t,r)=>t in e?Y(e,t,{enumerable:!0,configurable:!0,writable:!0,value:r}):e[t]=r,Q=(e,t)=>{for(var r in t||(t={}))K.call(t,r)&&g(e,r,t[r]);if(f)for(var r of f(t))z.call(t,r)&&g(e,r,t[r]);return e},X=JSON.parse('{"icon.title.alert-error":"Rød åttekant med hvitt utropstegn"}'),ee=JSON.parse('{"icon.title.alert-error":"Red octagon with white exclamation point"}'),te=JSON.parse('{"icon.title.alert-error":"Punainen kahdeksankulmio, jonka sisällä on valkoinen huutomerkki"}'),re=["en","nb","fi"],R="en",w=e=>re.find(t=>e===t||e.toLowerCase().includes(t))||R;function ne(){if(typeof window>"u"){const e=U.NMP_LANGUAGE||Intl.DateTimeFormat().resolvedOptions().locale;return w(e)}try{const e=document.documentElement.lang;return w(e)}catch(e){return console.warn("could not detect locale, falling back to source locale",e),R}}var ae=(e,t,r,a)=>e==="nb"?r:e==="fi"?a:t,ie=(e,t,r)=>{const a=ne(),l=ae(a,e,t,r);o.load(a,l),o.activate(a)};ie(ee,X,te);var oe=o.t({message:"Red octagon with white exclamation point",id:"icon.title.alert-error",comment:"Title for alert error icon"}),le=e=>s.createElement("svg",Q({xmlns:"http://www.w3.org/2000/svg",width:"16",height:"16",fill:"none",viewBox:"0 0 16 16",dangerouslySetInnerHTML:{__html:`<title>${oe}</title><path fill="currentColor" d="M4.1.586A2 2 0 0 1 5.516 0h4.97A2 2 0 0 1 11.9.586L15.413 4.1A2 2 0 0 1 16 5.514v4.97a2 2 0 0 1-.586 1.415L11.9 15.413a2 2 0 0 1-1.415.586h-4.97a2 2 0 0 1-1.414-.586L.586 11.9A2 2 0 0 1 0 10.485v-4.97A2 2 0 0 1 .586 4.1L4.1.586Z"></path><path fill="#fff" fill-rule="evenodd" d="M8 3.25a.75.75 0 0 1 .75.75v5a.75.75 0 1 1-1.5 0V4A.75.75 0 0 1 8 3.25Z" clip-rule="evenodd"></path><path fill="#fff" d="M8.8 11.8a.8.8 0 1 1-1.6 0 .8.8 0 0 1 1.6 0Z"></path>`}},e)),se=le,ce={},de=Object.defineProperty,x=Object.getOwnPropertySymbols,pe=Object.prototype.hasOwnProperty,he=Object.prototype.propertyIsEnumerable,y=(e,t,r)=>t in e?de(e,t,{enumerable:!0,configurable:!0,writable:!0,value:r}):e[t]=r,ue=(e,t)=>{for(var r in t||(t={}))pe.call(t,r)&&y(e,r,t[r]);if(x)for(var r of x(t))he.call(t,r)&&y(e,r,t[r]);return e},ve=JSON.parse('{"icon.title.alert-info":"Blå sirkel med bokstaven \\"I\\""}'),me=JSON.parse('{"icon.title.alert-info":"Blue circle with the letter I"}'),fe=JSON.parse('{"icon.title.alert-info":"Ympyrä, jonka sisällä on i-kirjain"}'),ge=["en","nb","fi"],G="en",_=e=>ge.find(t=>e===t||e.toLowerCase().includes(t))||G;function we(){if(typeof window>"u"){const e=ce.NMP_LANGUAGE||Intl.DateTimeFormat().resolvedOptions().locale;return _(e)}try{const e=document.documentElement.lang;return _(e)}catch(e){return console.warn("could not detect locale, falling back to source locale",e),G}}var xe=(e,t,r,a)=>e==="nb"?r:e==="fi"?a:t,ye=(e,t,r)=>{const a=we(),l=xe(a,e,t,r);o.load(a,l),o.activate(a)};ye(me,ve,fe);var _e=o.t({message:"Blue circle with the letter I",id:"icon.title.alert-info",comment:"Title for alert info icon"}),je=e=>s.createElement("svg",ue({xmlns:"http://www.w3.org/2000/svg",width:"16",height:"16",fill:"none",viewBox:"0 0 16 16",dangerouslySetInnerHTML:{__html:`<title>${_e}</title><circle cx="8" cy="8" r="8" fill="currentColor"></circle><path fill="#fff" fill-rule="evenodd" d="M8 4a1 1 0 1 0 0 2 1 1 0 0 0 0-2ZM7.25 12a.75.75 0 0 0 1.5 0V8a.75.75 0 0 0-1.5 0v4Z" clip-rule="evenodd"></path>`}},e)),$e=je,Se={},Oe=Object.defineProperty,j=Object.getOwnPropertySymbols,Ae=Object.prototype.hasOwnProperty,Ne=Object.prototype.propertyIsEnumerable,$=(e,t,r)=>t in e?Oe(e,t,{enumerable:!0,configurable:!0,writable:!0,value:r}):e[t]=r,Ie=(e,t)=>{for(var r in t||(t={}))Ae.call(t,r)&&$(e,r,t[r]);if(j)for(var r of j(t))Ne.call(t,r)&&$(e,r,t[r]);return e},Pe=JSON.parse('{"icon.title.alert-success":"Grønn sirkel med sjekkmerke"}'),Le=JSON.parse('{"icon.title.alert-success":"Green circle with checkmark"}'),be=JSON.parse('{"icon.title.alert-success":"Vihreä ympyrä, jonka sisällä on valintamerkki"}'),ke=["en","nb","fi"],J="en",S=e=>ke.find(t=>e===t||e.toLowerCase().includes(t))||J;function Ee(){if(typeof window>"u"){const e=Se.NMP_LANGUAGE||Intl.DateTimeFormat().resolvedOptions().locale;return S(e)}try{const e=document.documentElement.lang;return S(e)}catch(e){return console.warn("could not detect locale, falling back to source locale",e),J}}var Te=(e,t,r,a)=>e==="nb"?r:e==="fi"?a:t,Ce=(e,t,r)=>{const a=Ee(),l=Te(a,e,t,r);o.load(a,l),o.activate(a)};Ce(Le,Pe,be);var Me=o.t({message:"Green circle with checkmark",id:"icon.title.alert-success",comment:"Title for alert success icon"}),Ve=e=>s.createElement("svg",Ie({xmlns:"http://www.w3.org/2000/svg",width:"16",height:"16",fill:"none",viewBox:"0 0 16 16",dangerouslySetInnerHTML:{__html:`<title>${Me}</title><circle cx="8" cy="8" r="8" fill="currentColor" transform="rotate(180 8 8)"></circle><path fill="#fff" fill-rule="evenodd" d="M11.498 4.94a.75.75 0 0 1 .063 1.058l-4 4.5a.75.75 0 0 1-1.091.032l-2-2a.75.75 0 0 1 1.06-1.06l1.438 1.437 3.471-3.905a.75.75 0 0 1 1.06-.063Z" clip-rule="evenodd"></path>`}},e)),We=Ve,Be={},Re=Object.defineProperty,O=Object.getOwnPropertySymbols,Ge=Object.prototype.hasOwnProperty,Je=Object.prototype.propertyIsEnumerable,A=(e,t,r)=>t in e?Re(e,t,{enumerable:!0,configurable:!0,writable:!0,value:r}):e[t]=r,He=(e,t)=>{for(var r in t||(t={}))Ge.call(t,r)&&A(e,r,t[r]);if(O)for(var r of O(t))Je.call(t,r)&&A(e,r,t[r]);return e},Ze=JSON.parse('{"icon.title.alert-warning":"Gul varseltrekant med utropstegn"}'),Fe=JSON.parse('{"icon.title.alert-warning":"Yellow warning triangle with exclamation point"}'),qe=JSON.parse('{"icon.title.alert-warning":"Keltainen varoituskolmio, jonka sisällä on huutomerkki"}'),De=["en","nb","fi"],H="en",N=e=>De.find(t=>e===t||e.toLowerCase().includes(t))||H;function Ue(){if(typeof window>"u"){const e=Be.NMP_LANGUAGE||Intl.DateTimeFormat().resolvedOptions().locale;return N(e)}try{const e=document.documentElement.lang;return N(e)}catch(e){return console.warn("could not detect locale, falling back to source locale",e),H}}var Ye=(e,t,r,a)=>e==="nb"?r:e==="fi"?a:t,Ke=(e,t,r)=>{const a=Ue(),l=Ye(a,e,t,r);o.load(a,l),o.activate(a)};Ke(Fe,Ze,qe);var ze=o.t({message:"Yellow warning triangle with exclamation point",id:"icon.title.alert-warning",comment:"Title for alert warning icon"}),Qe=e=>s.createElement("svg",He({xmlns:"http://www.w3.org/2000/svg",width:"16",height:"16",fill:"none",viewBox:"0 0 16 16",dangerouslySetInnerHTML:{__html:`<title>${ze}</title><path fill="currentColor" d="M.245 12.01 6.159 1.083c.782-1.445 2.9-1.445 3.682 0l5.914 10.925c.734 1.357-.271 2.991-1.84 2.991H2.084C.517 15-.49 13.366.246 12.01Z"></path><path fill="#fff" fill-rule="evenodd" d="M8 3.25a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0V4A.75.75 0 0 1 8 3.25Z" clip-rule="evenodd"></path><path fill="#fff" d="M8.8 11.8a.8.8 0 1 1-1.6 0 .8.8 0 0 1 1.6 0Z"></path>`}},e)),Xe=Qe;function i({show:e,type:t,role:r="alert",children:a,...l}){const Z=c[t],F=c[`${t}Icon`];return n.jsx(D,{show:e,children:n.jsxs("div",{role:r,className:q(l.className,`${c.alert} ${Z}`),style:l.style,children:[n.jsx("div",{className:`${c.icon} ${F}`,children:et[t]}),n.jsx("div",{className:c.textWrapper,children:a})]})})}const et={negative:n.jsx(se,{}),positive:n.jsx(We,{}),warning:n.jsx(Xe,{}),info:n.jsx($e,{})};try{i.displayName="Alert",i.__docgenInfo={description:"",displayName:"Alert",props:{show:{defaultValue:null,description:"Determines whether the alert should be visible",name:"show",required:!1,type:{name:"Boolean"}},type:{defaultValue:null,description:"Type of alert",name:"type",required:!0,type:{name:"enum",value:[{value:'"negative"'},{value:'"info"'},{value:'"positive"'},{value:'"warning"'}]}},role:{defaultValue:{value:"alert"},description:'ARIA live region "role" attribute value',name:"role",required:!1,type:{name:"string"}},className:{defaultValue:null,description:"Additional classes to include",name:"className",required:!1,type:{name:"string"}},style:{defaultValue:null,description:"Additional CSS styles for the container.",name:"style",required:!1,type:{name:"CSSProperties"}},id:{defaultValue:null,description:"id used for relationship ARIA attributes",name:"id",required:!1,type:{name:"string"}}}}}catch{}const pt={title:"FeedbackIndicators/Alert"},d=()=>n.jsxs("div",{className:"flex flex-col gap-y-16",children:[n.jsxs("div",{children:[n.jsx("h3",{children:"Negative"}),n.jsx(i,{type:"negative",show:!0,children:'This is "negative" variant of the alert component'})]}),n.jsxs("div",{children:[n.jsx("h3",{children:"Positive"}),n.jsx(i,{type:"positive",show:!0,role:"status",children:'This is "positive" variant of the alert component'})]}),n.jsxs("div",{children:[n.jsx("h3",{children:"Warning"}),n.jsx(i,{type:"warning",show:!0,children:'This is "warning" variant of the alert component'})]}),n.jsxs("div",{children:[n.jsx("h3",{children:"Info"}),n.jsx(i,{type:"info",show:!0,role:"status",children:'This is "info" variant of the alert component'})]})]}),p=({type:e})=>n.jsxs(n.Fragment,{children:[n.jsxs("h4",{className:"h5",children:["This is ",e," variant of the alert component"]}),n.jsx("p",{children:"With an additional description"}),n.jsxs("div",{className:"mt-8 space-x-8",children:[n.jsx(m,{small:!0,children:"Primary CTA"}),n.jsx(m,{small:!0,secondary:!0,quiet:!0,children:"Secondary"})]})]}),h=()=>n.jsxs("div",{className:"flex flex-col gap-y-16",children:[n.jsxs("div",{children:[n.jsx("h3",{children:"Negative"}),n.jsx(i,{type:"negative",show:!0,children:n.jsx(p,{type:"negative"})})]}),n.jsxs("div",{children:[n.jsx("h3",{children:"Positive"}),n.jsx(i,{type:"positive",show:!0,role:"status",children:n.jsx(p,{type:"positive"})})]}),n.jsxs("div",{children:[n.jsx("h3",{children:"Warning"}),n.jsx(i,{type:"warning",show:!0,children:n.jsx(p,{type:"warning"})})]}),n.jsxs("div",{children:[n.jsx("h3",{children:"Info"}),n.jsx(i,{type:"info",show:!0,role:"status",children:n.jsx(p,{type:"info"})})]})]}),u=()=>{const[e,t]=s.useState(!0);return n.jsxs(n.Fragment,{children:[n.jsx("h3",{children:'Example with default role="alert"'}),n.jsx(m,{className:"mb-16",small:!0,primary:!0,onClick:()=>{t(!e)},"aria-controls":"negative-alert","aria-expanded":e,children:e?"Hide negative alert":"Show negative alert"}),n.jsxs(i,{id:"negative-alert",type:"negative",show:e,children:[n.jsx("h4",{className:"h5",children:'This is "negative" variant of the alert component'}),n.jsx("p",{children:"With an additional description"})]})]})},v=()=>{const[e,t]=s.useState(!0);return n.jsxs(n.Fragment,{children:[n.jsx("h3",{children:'Example with role="alert" on a descendant element'}),n.jsx(m,{className:"mb-16",small:!0,primary:!0,onClick:()=>{t(!e)},"aria-controls":"overriden-role-example-alert","aria-expanded":e,children:e?"Hide warning alert":"Show warning alert"}),n.jsxs(i,{id:"overriden-role-example-alert",type:"warning",show:e,role:"",children:[n.jsx("h4",{className:"h5",children:'This is "warning" variant of the alert component'}),n.jsx("p",{children:"With an additional description that is not that important"})]})]})};var I,P,L;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`() => <div className="flex flex-col gap-y-16">
+    <div>
+      <h3>Negative</h3>
+      <Alert type="negative" show>
+        This is "negative" variant of the alert component
+      </Alert>
+    </div>
+    <div>
+      <h3>Positive</h3>
+      <Alert type="positive" show role="status">
+        This is "positive" variant of the alert component
+      </Alert>
+    </div>
+    <div>
+      <h3>Warning</h3>
+      <Alert type="warning" show>
+        This is "warning" variant of the alert component
+      </Alert>
+    </div>
+    <div>
+      <h3>Info</h3>
+      <Alert type="info" show role="status">
+        This is "info" variant of the alert component
+      </Alert>
+    </div>
+  </div>`,...(L=(P=d.parameters)==null?void 0:P.docs)==null?void 0:L.source}}};var b,k,E;h.parameters={...h.parameters,docs:{...(b=h.parameters)==null?void 0:b.docs,source:{originalSource:`() => <div className="flex flex-col gap-y-16">
+    <div>
+      <h3>Negative</h3>
+      <Alert type="negative" show>
+        <InteractiveContent type="negative" />
+      </Alert>
+    </div>
+    <div>
+      <h3>Positive</h3>
+      <Alert type="positive" show role="status">
+        <InteractiveContent type="positive" />
+      </Alert>
+    </div>
+    <div>
+      <h3>Warning</h3>
+      <Alert type="warning" show>
+        <InteractiveContent type="warning" />
+      </Alert>
+    </div>
+    <div>
+      <h3>Info</h3>
+      <Alert type="info" show role="status">
+        <InteractiveContent type="info" />
+      </Alert>
+    </div>
+  </div>`,...(E=(k=h.parameters)==null?void 0:k.docs)==null?void 0:E.source}}};var T,C,M;u.parameters={...u.parameters,docs:{...(T=u.parameters)==null?void 0:T.docs,source:{originalSource:`() => {
+  const [show, setShow] = React.useState(true);
+  return <>
+      <h3>Example with default role="alert"</h3>
+      <Button className="mb-16" small primary onClick={() => {
+      setShow(!show);
+    }} aria-controls="negative-alert" aria-expanded={show}>
+          {show ? 'Hide negative alert' : 'Show negative alert'}
+        </Button>
+
+      <Alert id="negative-alert" type="negative" show={show}>
+        <h4 className="h5">This is "negative" variant of the alert component</h4>
+        <p>With an additional description</p>
+      </Alert>
+    </>;
+}`,...(M=(C=u.parameters)==null?void 0:C.docs)==null?void 0:M.source}}};var V,W,B;v.parameters={...v.parameters,docs:{...(V=v.parameters)==null?void 0:V.docs,source:{originalSource:`() => {
+  const [show, setShow] = React.useState(true);
+  return <>
+      <h3>Example with role="alert" on a descendant element</h3>
+      <Button className="mb-16" small primary onClick={() => {
+      setShow(!show);
+    }} aria-controls="overriden-role-example-alert" aria-expanded={show}>
+        {show ? 'Hide warning alert' : 'Show warning alert'}
+      </Button>
+      <Alert id="overriden-role-example-alert" type="warning" show={show} role="">
+        <h4 className="h5">This is "warning" variant of the alert component</h4>
+        <p>With an additional description that is not that important</p>
+      </Alert>
+    </>;
+}`,...(B=(W=v.parameters)==null?void 0:W.docs)==null?void 0:B.source}}};const ht=["Variants","WithInteractiveContent","WithDefaultRole","WithOverriddenRole"];export{d as Variants,u as WithDefaultRole,h as WithInteractiveContent,v as WithOverriddenRole,ht as __namedExportsOrder,pt as default};
