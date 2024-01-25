9c00888b5e55f5871b4f884fa5ad7a2090e98ae1
William Killerud <william.killerud@schibsted.com>
2024-01-25 14:39:39 +0100

    fix: reference useId in a way compatible with React 17 (#197)

--- packages/utils/src/useId.ts
+++ packages/utils/src/useId.ts
@@ -1,9 +1,16 @@
-import { useId as reactUseId } from 'react';
+import React from 'react';
+
+const useReactId = React.useId;
 
 export const useId = (hasFallback?): string => {
-  // reactUseId returns a string that includes colons (:), e.g., :r0:, :r1:, etc.
-  // This string is NOT supported in CSS selectors. Hence the replace.
-  return hasFallback ?? reactUseId().replace(/:/g, '');
+  if (typeof useReactId === 'function') {
+    // reactUseId returns a string that includes colons (:), e.g., :r0:, :r1:, etc.
+    // This string is NOT supported in CSS selectors. Hence the replace.
+    return hasFallback ?? useReactId().replace(/:/g, '');
+  } else {
+    const [elId] = React.useState(generateId());
+    return elId;
+  }
 };
 
 /**
