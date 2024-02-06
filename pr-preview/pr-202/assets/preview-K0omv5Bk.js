428dc8951e784721744e5cf8e1431c753bd01e42
imprashast <imprashast@users.noreply.github.com>
2024-02-06 13:59:04 +0000

    Deploy preview for PR 202 🛫

--- pr-preview/pr-202/assets/preview-K0omv5Bk.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-Yd2Fo0gy.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-EDRLV1zP.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-EDRLV1zP.js","./iframe-Yd2Fo0gy.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./client-O7qhN-hJ.js","./inheritsLoose-lRRTvJTO.js","./_getPrototype-L45pWGvc.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
